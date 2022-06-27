9d623653e2f35509a5dca168b22b1e40bb17e064
martinmj4im <63520006+martinmj4im@users.noreply.github.com>
2022-06-26 19:16:40 -0600

    navbar done

---
+++ airbnb/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
